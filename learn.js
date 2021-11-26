095501191a8b94baf7d1476669e2e4534af876f8
ebedinin <bedinin@gmail.com>
2021-11-27 08:48:11 +1000

    31

--- learn.js
+++ learn.js
@@ -1,10 +1,12 @@
-const findLongestWordLength = (str) => {
-    let length = 0;
-    const arr = str.split(" ");
-    for(let index in arr){
-        if (length < arr[index].length) length = arr[index].length;
+function largestOfFour(arr) {
+    const arrSubMax = [];
+    for (let i = 0; i < arr.length; i++) {
+        let max = arr[i][0];
+        for (let j = 1; j < arr[i].length; j++)
+            if (max < arr[i][j]) max = arr[i][j];
+        arrSubMax.push(max);
     }
-    return length;
-};
+    return arrSubMax;
+}
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
