7d5786f895a8da479cc1164608b36039fba41c56
ebedinin <bedinin@gmail.com>
2021-11-27 15:03:41 +1000

    88

--- learn.js
+++ learn.js
@@ -1,7 +1,12 @@
-function bouncer(arr) {
-    return arr.filter((item)=>{
-        return Boolean(item);
-    });
+function getIndexToIns(arr, num) {
+    arr = arr.sort((a, b) => a - b);
+    if (arr.length === 0) return 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= num) return i;
+    }
+    return arr.length;
 }
 
-console.log(bouncer([7, "ate", "", false, 9]));
+
+console.log(getIndexToIns([5, 3, 20, 3], 5));
+console.log(getIndexToIns([2, 20, 10], 19));
